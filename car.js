7cd92c175485054c2d6fa1f29fb05aff3bdb1f94
AYA MHATLI <eyamhetli@gmail.com>
2022-12-12 09:39:15 +0100

    nouveau

---
+++ car.js
@@ -0,0 +1,38 @@
+class Car{
+    //  Hashcode="";
+      constructor() {
+          this.price = [];
+         // Hashcode=this.Hashcode;
+          
+      }
+  
+      list() {
+          return [...this.price];
+      }
+  
+      add(title) {
+          let todo = {
+              title: title,
+              completed: false,
+          }
+  
+          this.price.push(todo);
+      }
+      complete(title) {
+          let todoFound = false;
+          this.price.forEach((todo) => {
+              if (todo.title === title) {
+                  todo.completed = true;
+                  todoFound = true;
+                  return;
+              }
+          });
+  
+          if (!todoFound) {
+              throw new Error(`No TODO was found with the title: "${title}"`);
+          }
+      }
+  
+  }
+  module.exports = Car;
+  
